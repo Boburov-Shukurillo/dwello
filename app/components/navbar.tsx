75bc86d99a291d4a36724fa24428cf8ee5b1d815
Boburov-Shukurillo <boburovshukurilo@gmail.com>
2025-04-18 08:20:56 +0500

    finished hero page

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,6 +5,8 @@ import "../styles/globals.css";
 
 import lgog from "../imgs/logo.svg";
 import schic from "../imgs/search.svg";
+import profile_ic from "../imgs/user.svg";
+
 export default function Navbar() {
   return (
     <nav className="conatinerb flex justify-between py-5">
@@ -12,39 +14,40 @@ export default function Navbar() {
         <Image src={lgog} width={100} height={29} alt="Picture of the author" />
       </Link>
 
-      <div className="flex items-center justify-between">
-        <ul className="flex items-center gap-16">
-          <li className="h-5">
-            <Link href="/" className="hover:opacity-80">
-              Home
-            </Link>
-          </li>
-          <li className="h-5">
-            <Link href="/service" className="hover:opacity-80">
-              Service
-            </Link>
-          </li>
-          <li className="h-5">
-            <Link href="/agents" className="hover:opacity-80">
-              Agents
-            </Link>
-          </li>
-          <li className="h-5">
-            <Link href="/contact" className="hover:opacity-80">
-              Contact
-            </Link>
-          </li>
-        </ul>
+      <ul className="w-1/4 flex items-center gap-16 font-[montexbold] leading-2">
+        <li>
+          <Link href="/" className="hover:opacity-80">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/service" className="hover:opacity-80">
+            Service
+          </Link>
+        </li>
+        <li>
+          <Link href="/agents" className="hover:opacity-80">
+            Agents
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact" className="hover:opacity-80">
+            Contact
+          </Link>
+        </li>
+      </ul>
+      <div className="w-1/5 flex items-center justify-between">
+        <Image src={schic} width={24} height={24} alt="Picture of the author" />
+        <Image
+          src={profile_ic}
+          width={24}
+          height={24}
+          alt="Picture of the login"
+        />
 
-        <label htmlFor="search" className="flex items-end">
-          <input type="text" id="search" name="search" className="bg-" />
-          <Image
-            src={schic}
-            width={20}
-            height={20}
-            alt="Picture of the author"
-          />
-        </label>
+        <button className="bg-[#2B1B12] w-32 h-11 rounded-lg text-white">
+          Sign In
+        </button>
       </div>
     </nav>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
+import hoseic from './imgs/heroimg.png'
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="conatinerb flex items-center justify-between pt-16">
+      <div className="w-1/2">
+        <h1 className="text-[68px] leading-16 mb-12 ">
+          Find Your <br /> Dream Home
+        </h1>
+        <p className="w-2/3 text-lg font-[montbold] leading-[160%] mb-20">
+          Explore our curated selection of exquisite properties meticulously
+          tailored to your unique dream home vision
+        </p>
 
-export default page
+        <button className="bg-[#2B1B12] w-44 h-14 text-lg rounded-lg text-white">
+          Sign In
+        </button>
+      </div>
+      <Image
+        src={hoseic}
+        width={740}
+        height={440}
+        alt="Picture of the login"
+      />
+    </div>
+  );
+};
+
+export default page;
